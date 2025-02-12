dd860eb923a2ee5fc4d07c743ece84b1b8aa9745
pixel-farmer <cassie@pixel-farmers.com>
2025-02-11 19:36:01 -0500

    Update Vite and Vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
